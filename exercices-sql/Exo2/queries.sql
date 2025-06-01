5ba94b560eed5e9e6257dacf8047469081697089
mt👾💻📈 <benimadidi100@gmail.com>
2025-06-01 19:13:29 +0100

    Ajout de l'exercice numéro 2

--- exercices-sql/Exo2/queries.sql
+++ exercices-sql/Exo2/queries.sql
@@ -20,3 +20,34 @@
     (6) Afficher les objets qui sont ni vendables, ni echangéables
 */
 
+
+-- (1) Afficher la liste des classes du jeu
+SHOW TABLES;
+SELECT * FROM `classes`;
+
+--(2) Afficher cette même liste, mais triée par ordre alphabétique sur le nom 
+SELECT *
+FROM  `classes`
+ORDER BY `class_name`;
+
+-- (3) Afficher les personnages de l'identifiant 8 (inclus) à 13 (inclus)
+SELECT * 
+FROM `classes`
+ORDER BY `class_id`
+LIMIT 13 OFFSET 7;
+
+-- (4) Afficher le nom des objets qui contiennent le terme "Recette"
+SELECT *
+FROM `items`
+WHERE `item_name` LIKE "Recette%";
+
+-- (5) Afficher le nom et le niveau des personnages qui ont atteint le niveau 70 (tri croissant sur le nom)
+SELECT `character_name` AS 'Nom du joueur', `character_level` AS 'Niveau du joueur'
+FROM `characters`
+WHERE `character_level` = 70
+ORDER BY `character_name` ASC;
+
+-- (6) Afficher les objets qui sont ni vendables, ni echangéables
+SELECT * 
+FROM `items`
+WHERE `item_tradeable` IS NULL;

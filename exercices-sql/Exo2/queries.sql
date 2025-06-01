9011954d9475e3556fa6d6ddf83328eb3c2e14d8
mt👾💻📈 <benimadidi100@gmail.com>
2025-06-01 18:36:44 +0100

    Modification du chemin vers le fichier source de la base de données de wow

---
+++ exercices-sql/Exo2/queries.sql
@@ -0,0 +1,22 @@
+ 
+/*
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    EXO SQL #2
+    [Révision : Lecture et filtrage de données]
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    SOURCE C:/Users/hp/Desktop/Vs_code/Exercices-prog/exercices-sql/Exo2/wow_data_v1.sql
+
+    // INDICATIONS : 
+    //  - Définissez des alias aux colonnes affichées dans les résultats de vos requêtes (sauf si SELECT *).
+    //  - Ne consulter pas le fichier de la base de données (utilisez des requêtes pour chercher des informations).
+
+
+    (1) Afficher la liste des classes du jeu 
+    (2) Afficher cette même liste, mais triée par ordre alphabétique sur le nom 
+    (3) Afficher les personnages de l'identifiant 8 (inclus) à 13 (inclus)
+    (4) Afficher le nom des objets qui contiennent le terme "Recette"
+    (5) Afficher le nom et le niveau des personnages qui ont atteint le niveau 70 (tri croissant sur le nom)
+    (6) Afficher les objets qui sont ni vendables, ni echangéables
+*/
+

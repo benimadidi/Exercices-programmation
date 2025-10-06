738292c6016c7fcf60b9dd05d5f0cc19091e31e5
mt👾💻📈 <benimadidi100@gmail.com>
2025-10-06 14:07:13 +0100

    Debut d'un projet pour apprentissage de react + tailwindcss

--- exercices-frameworks/ReactJS/liste_de_produit/products/productrow.jsx
+++ exercices-frameworks/ReactJS/liste_de_produit/products/productrow.jsx
@@ -4,9 +4,13 @@
  * @param {{product: {name: string, price: number, stocked: boolean}}} product 
  */
 
+import { useEffect } from "react"
+
 export function ProductRow({product}) {
 
     const style = product.stocked ? undefined : {color: 'red'}
+
+    throw new Error('azea')
 
     return <tr>
         <td style={style}>{product.name}</td>

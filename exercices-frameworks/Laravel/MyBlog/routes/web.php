dc46331ab6c4aa0ca4fbeadb722e9eb3a5ffb976
mt👾💻📈 <benimadidi100@gmail.com>
2026-01-03 21:17:30 +0100

    feat(backend) : Ajout d'un projet Laravel avec react + inertia. Mis en place du backend

---
+++ exercices-frameworks/Laravel/MyBlog/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::middleware(['auth', 'verified'])->group(function () 
+{
+
+    Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+    Route::post('/post/{post}/like', [PostController::class, 'like'])->name('post.like');
+
+});
+
+Route::get('post/{post}', [PostController::class, 'show'])->name('post.show');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('dashboard', [
+        'usersPosts' => Auth::user()->posts()->with('author')->latest()->get()
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+require __DIR__.'/settings.php';
